bed2d0a4c039dc32dfce2b2f556df4c95074da01
Caroline Dallmann <caroline.dallmann@capgemini.com>
2022-05-12 21:14:40 -0300

    inclusão release

--- materia.js
+++ materia.js
@@ -11,6 +11,7 @@ let endpointMaterias = (app, pool) => {
                     return res.status(401).send({msg:'Não autorizado'})
                 }
                 res.status(200).send(result.rows)
+                client.release()
             })
         })
     })
@@ -22,9 +23,10 @@ let endpointMaterias = (app, pool) => {
             }
             client.query('select * from materia where id = $1', [req.params.id], (err, result) => {
                 if (err) {
-                    return res.status(401).send({msg:'Não autorizado'})
+                    return res.status(401).send({msg:'Não encontrado'})
                 }
                 res.status(200).send(result.rows[0])
+                client.release()
             })
         })
     })
@@ -43,9 +45,10 @@ let endpointMaterias = (app, pool) => {
                 }
                 client.query('insert into materia(nome) values($1)', [req.body.nome], (err, result) => {
                     if (err) {
-                        return res.status(401).send({msg:'Não autorizado'})
+                        return res.status(401).send({msg:'Não foi possível salvar'})
                     }
                     res.status(201).send({msg: 'Salvo com sucesso'})
+                    client.release()
                 })
             })
         })
@@ -58,11 +61,10 @@ let endpointMaterias = (app, pool) => {
             }
             client.query('update materia set nome=$1 where id = $2', [req.body.nome, req.params.id], (err, result) => {
                 if (err) {
-                    return res.status(401).send({msg:'Erro na atualização'})
+                    return res.status(401).send({msg:'Não foi possível alterar'})
                 }
                 res.status(200).send({msg:'Alterado com sucesso'})
-
-
+                client.release()
             })
         })
     })
@@ -77,6 +79,7 @@ let endpointMaterias = (app, pool) => {
                     return res.status(404).send({msg:'Matéria não existe'})
                 }
                 res.status(200).send({msg:'Excluído com sucesso'})
+                client.release()
             })
         })
     })
