d2c7301b468c59a39d0a6381d27c272c5a12fd3b
Caroline Dallmann <caroline.dallmann@capgemini.com>
2022-06-13 08:24:51 -0300

    implementação inicial swagger

--- backend.js
+++ backend.js
@@ -3,6 +3,9 @@ const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 8081
 
+const swaggerUi = require('swagger-ui-express'),
+    swaggerDocument = require('./swagger.json');
+
 
 require('dotenv').config()
 
@@ -21,6 +24,8 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 app.use(cors())
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 materia(app, pool)
 frequencia(app, pool)
